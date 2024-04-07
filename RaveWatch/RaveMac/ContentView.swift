5b91e228e9603cd93198332685aac49ac34a90f8
ReggieSean <yxuhuang@bu.edu>
2024-04-06 23:07:25 -0400

    vm transfer successful

--- RaveWatch/RaveMac/ContentView.swift
+++ RaveWatch/RaveMac/ContentView.swift
@@ -10,23 +10,14 @@ import RavePackages
 
 struct ContentView: View {
     @StateObject var vm = RavePackages.CBluetoothPeripherialVM()
-    @State private var timeRemaining = 30
-    @State private var counting = false
-    @State var timer = Timer.publish(every:1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         let _ = Self._printChanges()
         VStack {
-            Button(action:{counting = !counting}){
-                Text(counting ? "Stop advertising" : "Start advertising")
+            Button(action:{vm.advertising = !vm.advertising}){
+                Text(vm.advertising ? "Stop advertising" : "Start advertising")
             }
-            Text("Time left : \(timeRemaining)")
-        }.onReceive(timer){_ in
-            if(counting){
-                timeRemaining = ((timeRemaining - 1) + 30) % 30
-                timeRemaining = timeRemaining == 0 ?  30 : timeRemaining
-                print(timeRemaining)
-            }
+            Text("Time left : \(vm.timeRemaining)")
         }
         .padding()
     }
